f82645dd31b803688297b34a6b4d2b270c3393b5
Enid Okemwa <okemwaenid23@gmail.com>
2023-07-20 03:28:00 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - check if a character is a digit
+ * @x: The number to be checked
+ * Return: 1 for character that is a digit or 0 for anything else
+ */
+int _isdigit(int x)
+{
+if (x >= 0 && x <= 9)
+{
+return (1);
+}
+return (o);
+}
