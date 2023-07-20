819ca8f67b2308c472e306506fa4525740b16dd5
Enid Okemwa <okemwaenid23@gmail.com>
2023-07-20 03:41:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * null - multiplies two integers
+ * @x: first number to multiply
+ * @y: second number to multiply
+ * Return: result of the product
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
